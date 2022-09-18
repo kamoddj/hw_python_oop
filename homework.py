30f4e8f6d0f8b5af6f04f5a16632ba6830f2155e
kamoddj <113038359+kamoddj@users.noreply.github.com>
2022-09-18 15:12:53 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -10,10 +10,10 @@ class InfoMessage:
     speed: float
     calories: float
     message: str = ('Тип тренировки: {training_type}; '
-                     'Длительность: {duration:.3f} ч.; '
-                     'Дистанция: {distance:.3f} км; '
-                     'Ср. скорость: {speed:.3f} км/ч; '
-                     'Потрачено ккал: {calories:.3f}.')
+                    'Длительность: {duration:.3f} ч.; '
+                    'Дистанция: {distance:.3f} км; '
+                    'Ср. скорость: {speed:.3f} км/ч; '
+                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         """Возвращает строку сообщения"""
